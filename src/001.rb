b31d8189531c3b1291e4e28683b2de970c1d7b60
ryuichi1208 <ryucrosskey@gmail.com>
2021-08-12 09:33:08 +0900

    first commit

---
+++ src/001.rb
@@ -0,0 +1,33 @@
+d = 10
+e = 20
+print((d + e).to_s + "\n")
+
+str = "Tokyo"
+str << ",Japan"
+puts str
+
+calc1 = 20 * 15
+calc2 = 30 - 18
+print("20 * 15 = ", calc1, ", 30 - 18 = ", calc2,  "¥n")
+
+# コメント
+print("aaa\n") # コメント2
+
+=begin
+ドキュメント
+ドキュメント
+ドキュメント
+=end
+
+puts("Hello"); puts("World")
+
+a = String.new("def")
+puts a
+
+b = <<TEXT
+aaa
+bbb
+ccc
+TEXT
+
+puts "#{b}"
