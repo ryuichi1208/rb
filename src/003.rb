c65d00f584775787f993c56c0af98bc143e0335b
ryuichi1208 <ryucrosskey@gmail.com>
2021-08-12 10:03:23 +0900

    first commit

--- src/003.rb
+++ src/003.rb
@@ -11,3 +11,27 @@ until num <= 0 do
   num = num - 1
 end
 print("End")
+
+for num in 1..3 do
+  print("num = ", num, "¥n")
+end
+print("End")
+
+for n in 1..5 do
+  print("n=",n,"\n")
+end
+
+print("18..20 is ¥n")
+for num in 18..20 do
+  print("num = ", num, "¥n")
+end
+
+print("18...20 is ¥n")
+for num in 18...20 do
+  print("num = ", num, "¥n")
+end
+
+print("¥"Ax¥"..¥"Bc¥" is ¥n")
+for str in "Ax".."Bb" do
+  print("str = " + str + "¥n")
+end
